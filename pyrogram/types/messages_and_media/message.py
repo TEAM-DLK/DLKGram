9dcc011181a7b423a1c9156c2ddd1e43dcbf023b
RimMirK <me@rimmirk.pp.ua>
2025-06-13 14:40:31 +0200

    Fix return type docstring in Message.delete() (#186)

--- pyrogram/types/messages_and_media/message.py
+++ pyrogram/types/messages_and_media/message.py
@@ -5436,7 +5436,7 @@ class Message(Object, Update):
                 Defaults to True.
 
         Returns:
-            True on success, False otherwise.
+            ``int`` – Amount of affected messages.
 
         Raises:
             RPCError: In case of a Telegram RPC error.
