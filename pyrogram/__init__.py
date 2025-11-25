da76796c7b80c1b9e4d6cea38585723dc26eecba
DΣV <93440107+TEAM-DLK@users.noreply.github.com>
2025-11-25 08:43:52 +0530

    Update __init__.py

--- pyrogram/__init__.py
+++ pyrogram/__init__.py
@@ -18,7 +18,7 @@
 
 __version__ = "2.2.6"
 __license__ = "GNU Lesser General Public License v3.0 (LGPL-3.0)"
-__copyright__ = "Copyright (C) 2017-present Dan <https://github.com/delivrance>"
+__copyright__ = "Copyright (C) 2017-present Dan <https://github.com/TEAM-DLK>"
 
 from concurrent.futures.thread import ThreadPoolExecutor
 
